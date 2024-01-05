4b98d87047bf4c5ed8889800b0f41a6ddc29c399
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-01-05 07:00:45 +0000

    new Dates class

---
+++ Dates.java
@@ -0,0 +1,23 @@
+
+import java.time.LocalDate;
+
+class DatesClass {
+    private LocalDate dateLocalDate;
+    
+    public DatesClass() {
+        this.dateLocalDate = java.time.LocalDate.now();
+    }
+
+    public LocalDate getDateLocalDate() {
+        return this.dateLocalDate;
+    }
+
+}
+
+
+public class Dates {
+public static void main(String[] args) {
+    DatesClass dater = new DatesClass();
+
+    System.out.println(dater.getDateLocalDate());
+}}
