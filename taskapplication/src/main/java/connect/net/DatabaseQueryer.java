6ec87b1d1cf1b0094be4dcdc50cc684be8d5cb8a
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-01-06 11:04:48 +0000

    connect() will now create / connect to database

--- taskapplication/src/main/java/connect/net/DatabaseQueryer.java
+++ taskapplication/src/main/java/connect/net/DatabaseQueryer.java
@@ -5,47 +5,57 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ *
+ * @author sqlitetutorial.net
+ */
 public class DatabaseQueryer {
-
-    public static void main(String[] args) {
-        Connection connection = null;
+     /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
         try {
             // db parameters
-            String url = "jdbc:sqlite:/taskapplication/src/main/java/connect/net/goalsdatabase";
+            // C:\Users\micha\OneDrive\Projects\TaskApp-1.0\taskapplication\src\main\resources\db
+            String url = "jdbc:sqlite:C:/Users/micha/OneDrive/Projects/TaskApp-1.0/taskapplication/src/main/resources/db/goals";
             // create a connection to the database
-
-            // try {
-            //     Class.forName("org.sqlite.JDBC");
-            //  }
-            //  catch(ClassNotFoundException ex) {
-            //     System.out.println("Error: unable to load driver class!");
-            //     System.exit(1);
-            //  }
-
-            connection = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
             
             System.out.println("Connection to SQLite has been established.");
-
-            try (Statement statement = connection.createStatement()) {
-                String createTasksTable = "CREATE TABLE tasks (" +
-                        "task_id INT PRIMARY KEY," +
-                        "task_name VARCHAR(255) NOT NULL" +
-                        ");";
-                statement.executeUpdate(createTasksTable);
-            }
-
-            try (Statement statement = connection.createStatement()) {
-                String createTaskCompletionsTable = "CREATE TABLE task_completions (" +
-                        "completion_id INT PRIMARY KEY," +
-                        "task_id INT," +
-                        "completion_datetime DATETIME NOT NULL," +
-                        "FOREIGN KEY (task_id) REFERENCES tasks(task_id)" +
-                        ");";
-                statement.executeUpdate(createTaskCompletionsTable);
-            }
-
-            System.out.println("Tables created successfully.");
             
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public void createDatabase(){
+        try (Statement statement = connection.createStatement()) {
+            String createTasksTable = "CREATE TABLE tasks (" +
+                    "task_id INT PRIMARY KEY," +
+                    "task_name VARCHAR(255) NOT NULL" +
+                    ");";
+            statement.executeUpdate(createTasksTable);
+        }
+
+        try (Statement statement = connection.createStatement()) {
+            String createTaskCompletionsTable = "CREATE TABLE task_completions (" +
+                    "completion_id INT PRIMARY KEY," +
+                    "task_id INT," +
+                    "completion_datetime DATETIME NOT NULL," +
+                    "FOREIGN KEY (task_id) REFERENCES tasks(task_id)" +
+                    ");";
+            statement.executeUpdate(createTaskCompletionsTable);
+        
+        System.out.println("Tables created successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -58,4 +68,12 @@ public class DatabaseQueryer {
             }
         }
     }
-}
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect();
+        createDatabase();
+    }
